6adfe08a42ac2875fcd95dad6ac3715b470c7cd0
creaiter <qkrwlsqo94@gmail.com>
2021-09-02 06:30:18 +0000

    Update

--- assets/javascript/toggleDark.js
+++ assets/javascript/toggleDark.js
@@ -10,15 +10,3 @@ checkbox.addEventListener('change', function() {
         moon.innerHTML = feather.icons['moon'].toSvg({'fill': 'var(--fg-color-dark)'})
     }
 })
-
-function growDiv(div_id, bttn_id) {
-    var growDiv = document.getElementById(div_id);
-    if (growDiv.clientHeight) {
-      growDiv.style.height = 0;
-    } else {
-      var wrapper = growDiv.querySelector('.measuringWrapper');
-      //var wrapper = document.querySelector('.measuringWrapper');
-      growDiv.style.height = wrapper.clientHeight + "px";
-    }
-    document.getElementById(bttn_id).value=document.getElementById(bttn_id).value=='details'?'hide':'details';
-}
